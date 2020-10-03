aa54f239b9c6452432a8be41e61beed5c09f2659
1983LeMans956 <63455417+1983LeMans956@users.noreply.github.com>
2020-10-03 19:53:57 +0400

    feat:add result

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-
-exports.min = function min (array) {
-  return 0;
+ 
+exports.min = function min (array=[]) {
+  return array.length > 0  ? Math.min( ...array) : 0; 
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array=[]) {
+  return array.length > 0  ? Math.max( ...array) : 0; 
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array=[]) {
+  var res = 0;
+  for(var i = 0; i < array.length; i++) {
+    res += array[i];
+  }
+  return array.length > 0 ? res/array.length :0;
 }
